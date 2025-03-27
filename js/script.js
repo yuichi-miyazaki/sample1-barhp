458b95cbfafcaf2abe8d93bbdfe030decdf72dc2
ゆーいち【Webデザイン】 <64854666+yuichi-webdesign@users.noreply.github.com>
2025-03-28 00:15:39 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function changeBackground() {
         if (section) {
+            // 背景画像の切り替え
+            section.style.transition = "background-image 1s ease-in-out";  // 背景画像切り替え時にアニメーションを加える
             section.style.backgroundImage = `url(${images[currentIndex]})`;
+
+            // インデックス更新
             currentIndex = (currentIndex + 1) % images.length;
         }
     }
@@ -19,6 +23,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // 5秒ごとに背景画像を変更
     setInterval(changeBackground, 5000);
 
-    // 初回適用
+    // 初回適用時に背景画像を変更
     changeBackground();
 });
